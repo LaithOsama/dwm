d5921d1dfda9e52862a0deed1691b3d344a0a10e
Laith Osama <81252120+LaithOsama@users.noreply.github.com>
2022-01-23 22:54:22 +0300

    Update config.h

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=11", "Material Design Icons-Regular:size=11" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=10", "Material Design Icons-Regular:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size=10";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#fbf2c7";
