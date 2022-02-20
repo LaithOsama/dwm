9c444dcf545331757b3783f7ad4ab501fc8ec2d1
Laith Osama <81252120+LaithOsama@users.noreply.github.com>
2022-02-20 08:09:35 +0300

    Update config.h

--- config.h
+++ config.h
@@ -57,7 +57,8 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char[]){ "/bin/dash", "-c", cmd, NULL } }
+
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -65,6 +66,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont 
 static const char *termcmd[]  = { "st", NULL };
 static const char *webcmd[]  = { "firefox", NULL};
 static const char *filecmd[]  = { "pcmanfm", "/home/laith", NULL};
+static const char *rsscmd[]  = { "newsboat", NULL};
+
 
 //volume controls
 static const char *upvol[]   = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
@@ -78,6 +81,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = webcmd } },
 	{ MODKEY,                       XK_e,	   spawn,          {.v = filecmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = rsscmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
