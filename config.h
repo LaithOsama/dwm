f15ce58ed7d43037a97cdb52a99d34e3217d10c4
Laith Osama <81252120+LaithOsama@users.noreply.github.com>
2022-03-03 18:40:16 +0300

    Update config.h

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "","","ﱘ","","","</>" };
+static const char *tags[] = { "www","","ﱘ","","","</>" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont };
 static const char *termcmd[]  = { "st", NULL };
-static const char *webcmd[]   = { "firefox", NULL };
+static const char *webcmd[]   = { "qutebrowser", NULL };
 static const char *filecmd[]  = { "pcmanfm", "/home/laith", NULL };
 static const char *rsscmd[]   = { "st", "-c", "-e", "newsboat", NULL };
 static const char *brtdwncmd[] = { "xbacklight","-10", NULL };
@@ -84,15 +84,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_w,      spawn,          {.v = webcmd } },
 	{ MODKEY,                       XK_e,	     spawn,          {.v = filecmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = rsscmd } },
-  { ShiftMask,                    XK_Down,   spawn,          {.v = brtdwncmd } },
-  { ShiftMask,                    XK_Up,     spawn,          {.v = brtupcmd } },
+  	{ ShiftMask,                    XK_Down,   spawn,          {.v = brtdwncmd } },
+  	{ ShiftMask,                    XK_Up,     spawn,          {.v = brtupcmd } },
 	{ MODKEY,                       XK_F3,     spawn,          {.v = upvol   } },
-  { MODKEY,                       XK_F2,     spawn,          {.v = downvol } },
-  { MODKEY,                       XK_F4,     spawn,          {.v = mutevol } },
-  { MODKEY,                       XK_F1,     spawn,          {.v = mixercmd } },
-  { ShiftMask,                    XK_l,      spawn,          {.v = lockcmd } },
-  { MODKEY,				                XK_Print,	 spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
-  { ShiftMask,			              XK_Print,	 spawn,		SHCMD("maim -s | xclip -selection clipboard -t image/png") },
+  	{ MODKEY,                       XK_F2,     spawn,          {.v = downvol } },
+  	{ MODKEY,                       XK_F4,     spawn,          {.v = mutevol } },
+  	{ MODKEY,                       XK_F1,     spawn,          {.v = mixercmd } },
+  	{ ShiftMask,                    XK_l,      spawn,          {.v = lockcmd } },
+  	{ MODKEY,				                XK_Print,	 spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
+  	{ ShiftMask,			              XK_Print,	 spawn,		SHCMD("maim -s | xclip -selection clipboard -t image/png") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
